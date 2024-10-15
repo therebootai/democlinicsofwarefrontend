0b1668a99a1e50f3a47c56b481a0a8bdf25167dd
Achinta27 <achinta27sep1999@gmail.com>
2024-10-15 22:52:17 +0530

    create frontend

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "280px",
+      md: "760px",
+      lg: "1024px",
+      xlg: "1280px",
+      xl: "1440px",
+      xxl: "1780px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
