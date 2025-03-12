01b479b6a9cf025cdebfd6c50a04a73e9a51cf27
Achinta27 <achinta27sep1999@gmail.com>
2025-03-12 15:07:56 +0530

    modified

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,21 @@
 import axios from "axios";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const LoginPage = () => {
-  const [emailorphone, setEmailorphone] = useState("");
-  const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [generatedCaptcha, setGeneratedCaptcha] = useState("");
-
-  const [emailorphoneError, setEmailorphoneError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [roleError, setRoleError] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [otp, setOtp] = useState(new Array(6).fill(""));
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [step, setStep] = useState(1);
+  const otpRefs = useRef([]);
 
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
 
   const { user, setUser, setFavClinic, setClinics } = useContext(AuthContext);
-
-  useEffect(() => {
-    generateCaptcha();
-  }, []);
 
   useEffect(() => {
     if (!!user._id) {
@@ -30,85 +23,103 @@ const LoginPage = () => {
     }
   }, [user]);
 
-  const generateCaptcha = () => {
-    const chars =
-      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let captcha = "";
-    for (let i = 0; i < 6; i++) {
-      captcha += chars[Math.floor(Math.random() * chars.length)];
-    }
-    setGeneratedCaptcha(captcha);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleCheckNumber = async () => {
     setLoading(true);
-    let isValid = true;
-
-    if (emailorphone === "") {
-      setEmailorphoneError("Email or phone is required.");
-      isValid = false;
-    } else {
-      setEmailorphoneError("");
-    }
-
-    if (password === "") {
-      setPasswordError("Password is required.");
-      isValid = false;
-    } else {
-      setPasswordError("");
-    }
-    if (role === "") {
-      setRoleError("Role is required.");
-      isValid = false;
-    } else {
-      setRoleError("");
-    }
-    if (!isValid) {
-      setLoading(false);
-      return;
-    }
-
+    setError("");
     try {
-      const response = await axios.post(
-        `${import.meta.env.VITE_BASE_URL}/api/user/login`,
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/user/checknumber`,
         {
-          emailOrPhone: emailorphone,
-          password,
-          role,
+          params: { phone: mobileNumber },
         }
       );
-      const { token, user } = response.data;
-      localStorage.setItem("token", token);
-      if (user.role !== "super_admin") {
-        setClinics(user.clinicId);
-        setFavClinic(user.clinicId[0]);
+
+      if (response.data.exists) {
+        sendOtp();
       } else {
-        const clinicResponse = await axios.get(
-          `${import.meta.env.VITE_BASE_URL}/api/clinic/all`
-        );
-        const clinicData = clinicResponse.data;
-        setClinics(clinicData);
-        setFavClinic(clinicData[0]);
+        alert("Phone number not found. Please enter a valid number.");
       }
-
-      setUser(user);
-
-      console.log("Login successful");
-      navigate("/doctor/dashboard");
     } catch (error) {
-      if (error.response && error.response.data) {
-        const errorMessage = error.response.data.message;
-        if (errorMessage.includes("Invalid credentials")) {
-          setEmailorphoneError("Invalid email or phone number.");
-          setPasswordError("Invalid password.");
+      setError("Error checking mobile number.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendOtp = async () => {
+    try {
+      setLoading(true);
+      setError("");
+
+      await axios.post(`${import.meta.env.VITE_BASE_URL}/api/user/send-otp`, {
+        phone: mobileNumber,
+      });
+
+      setStep(2);
+    } catch (error) {
+      setError("Failed to send OTP.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleOtpChange = (index, value) => {
+    if (!/^\d*$/.test(value)) return;
+
+    let newOtp = [...otp];
+    newOtp[index] = value;
+    setOtp(newOtp);
+
+    if (value !== "" && index < 5) {
+      otpRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleOtpBackspace = (index, e) => {
+    if (e.key === "Backspace" && index > 0) {
+      let newOtp = [...otp];
+      newOtp[index] = "";
+      setOtp(newOtp);
+      otpRefs.current[index - 1].focus();
+    }
+  };
+
+  const verifyOtp = async () => {
+    setLoading(true);
+    setError("");
+
+    const otpCode = otp.join("");
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/api/user/verify-with-otp`,
+        { phone: mobileNumber, otp: otpCode }
+      );
+
+      console.log("Backend Response:", response.data);
+
+      if (response.data.success) {
+        const { token, user } = response.data;
+        localStorage.setItem("token", token);
+        if (user.role !== "super_admin") {
+          setClinics(user.clinicId);
+          setFavClinic(user.clinicId[0]);
         } else {
-          setEmailorphoneError(errorMessage);
-          setPasswordError(errorMessage);
+          const clinicResponse = await axios.get(
+            `${import.meta.env.VITE_BASE_URL}/api/clinic/all`
+          );
+          const clinicData = clinicResponse.data;
+          setClinics(clinicData);
+          setFavClinic(clinicData[0]);
         }
+
+        setUser(user);
+
+        navigate("/doctor/dashboard");
       } else {
-        console.error("Login failed", error.message);
+        setError("Invalid OTP. Please try again.");
       }
+    } catch (error) {
+      setError("Error verifying OTP.");
     } finally {
       setLoading(false);
     }
@@ -118,104 +129,66 @@ const LoginPage = () => {
     <div className="flex justify-center h-screen bg-[#EDF4F7] overflow-y-scroll bg-no-repeat bg-cover bg-center overflow-x-hidden items-center ">
       <div className="lg:w-[45%] xlg:w-[40%] xl:w-[35%] w-[95%] md:w-[60%] bg-white h-fit py-10 lg:px-6 xl:px-16 gap-8 flex flex-col boxsh rounded-lg text-black bg-transparent">
         <div className="flex flex-col justify-center items-center  gap-4 ">
-          <img
-            src="/images/dentitydentallogo.png"
-            alt=""
-            className="h-[5rem] "
-          />
+          <img src="/images/demologo.svg" alt="" className="h-[3rem] " />
           <div className="xlg:text-lg text-base">
             Welcome back, Login to your account
           </div>
         </div>
 
-        <form
-          className="flex flex-col sm:gap-5 lg:gap-2 xl:gap-4"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col gap-2 ">
-            <label
-              className="xlg:text-lg text-base font-[400]"
-              htmlFor="emailOrPhone"
-            >
-              Mobile Number/ Email ID:
-            </label>
-            <input
-              type="text"
-              id="emailOrPhone"
-              value={emailorphone}
-              onChange={(e) => setEmailorphone(e.target.value)}
-              className="bg-[#EFEFEF] text-[#333333]  placeholder-[#00000033] rounded-sm xl:rounded-md h-[2.5rem] lg:h-[2.8rem] xl:h-[3rem] px-2 text-sm xl:text-lg outline-none"
-            />
-            {emailorphoneError && (
-              <div className="text-red-500 text-sm mt-1">
-                {emailorphoneError}
-              </div>
-            )}
-          </div>
-          <div className="flex flex-col  gap-2">
-            <label
-              className="xlg:text-lg text-base font-[400]"
-              htmlFor="password"
-            >
-              Password:
-            </label>
-            <div className="flex h-[2.5rem] lg:h-[2.8rem] xl:h-[3rem]  w-full items-center justify-between  rounded-sm xl:rounded-md bg-[#EFEFEF] ">
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="bg-[#EFEFEF]  rounded-sm xl:rounded-md h-[2.5rem] lg:h-[2.8rem] xl:h-[3rem] px-2 w-full text-[#333333]  placeholder-[#00000033] text-sm xl:text-lg border-none focus:outline-none"
-              />
-              <div
-                onClick={() => setShowPassword(!showPassword)}
-                className="mr-4 h-5 w-5 text-[#00000033] cursor-pointer"
+        <div className="flex flex-col sm:gap-5 lg:gap-2 xl:gap-4">
+          {step === 1 && (
+            <div className="flex flex-col gap-2 ">
+              <label
+                className="xlg:text-lg text-base font-[400]"
+                htmlFor="emailOrPhone"
               >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                Mobile Number
+              </label>
+              <input
+                type="text"
+                value={mobileNumber}
+                maxLength={10}
+                minLength={10}
+                onChange={(e) => setMobileNumber(e.target.value)}
+                className="bg-[#EFEFEF] text-[#333333]  placeholder-[#00000033] rounded-sm xl:rounded-md h-[2.5rem] lg:h-[2.8rem] xl:h-[3rem] px-2 text-sm xl:text-lg outline-none"
+              />
+              {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+              <button
+                onClick={handleCheckNumber}
+                className="w-full bg-[#FF2722] buttonshinehover transition-colors duration-300 hover:bg-blue-500 text-white p-2 mt-3 rounded"
+              >
+                {loading ? "Checking..." : "Send OTP"}
+              </button>
+            </div>
+          )}
+
+          {step === 2 && (
+            <>
+              <label className="block text-sm mb-2">Enter OTP</label>
+              <div className="flex justify-center gap-2">
+                {otp.map((digit, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    value={digit}
+                    maxLength="1"
+                    className="w-10 h-10 border-2 border-custom-blue rounded text-center text-lg focus:outline outline-green-800 "
+                    onChange={(e) => handleOtpChange(index, e.target.value)}
+                    onKeyDown={(e) => handleOtpBackspace(index, e)}
+                    ref={(el) => (otpRefs.current[index] = el)}
+                  />
+                ))}
               </div>
-            </div>
-            {passwordError && (
-              <div className="text-red-500 text-sm mt-1">{passwordError}</div>
-            )}
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="role" className="xlg:text-lg text-base font-[400]">
-              Choose Your Role
-            </label>
-            <select
-              name=""
-              value={role}
-              id="role"
-              onChange={(e) => setRole(e.target.value)}
-              className="bg-[#EFEFEF]  rounded-sm xl:rounded-md h-[2.5rem] lg:h-[2.8rem] xl:h-[3rem] px-2 w-full text-[#333333] placeholder-[#00000033] text-sm xl:text-lg border-none focus:outline-none"
-            >
-              <option value="">Choose Your Role</option>
-              <option value="doctor">Doctor</option>
-              <option value="staff">Staff</option>
-              <option value="super_admin">Admin</option>
-            </select>
-            {roleError && (
-              <div className="text-red-500 text-sm mt-1">{roleError}</div>
-            )}
-          </div>
-          <div className="flex w-full justify-between items-center">
-            <div className="flex flex-row gap-1 items-center xlg:text-lg text-base font-medium">
-              <input type="checkbox" name="" id="" />
-              <span>Remember me</span>
-            </div>
-            <div className="text-[#27B3FF] xlg:text-lg text-base font-medium">
-              Forgot password?
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-            <button
-              type="submit"
-              className="w-[30%] rounded xl:rounded-md sm:h-[2rem] xl:h-[2.5rem] flex justify-center items-center text-white text-base font-medium bg-[#27B3FF]"
-            >
-              {loading ? "Wait..." : "Login"}
-            </button>
-          </div>
-        </form>
+              {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+              <button
+                onClick={verifyOtp}
+                className="w-full bg-blue-500 buttonshinehover transition-colors duration-300 hover:bg-green-500 text-white p-2 mt-3 rounded"
+              >
+                {loading ? "Verifying..." : "Verify OTP"}
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
